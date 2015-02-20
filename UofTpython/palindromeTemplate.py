fa233d431be14d09976dfe42fc2c5090ba1abd72
HAKEON <hakeon@gmail.com>
2015-02-19 19:10:49 -0500

    initial commit

---
+++ UofTpython/palindromeTemplate.py
@@ -0,0 +1,12 @@
+def is_palindrome(s):
+    """ (str) -> bool
+
+    Return True if and only if x is a palindrome.
+
+    >>> is_palindrome('noon')
+    True
+    >>> is_palindrome('racecar')
+    True
+    >>> is_palindrome('dented')
+    False
+    """
